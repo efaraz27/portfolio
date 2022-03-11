74973f26d416a85ba0d6bc62aa8171acbeeaf66d
Ehtesham Faraz <efaraz27@gmail.com>
2022-03-12 00:29:58 +0530

    added recent experience

--- constants/experiencesList.js
+++ constants/experiencesList.js
@@ -1,7 +1,33 @@
 const experiencesList = [
   {
+    positionName: "Software Development Engineer Intern (Remote)",
+    date: "Jan 2021 - Present",
+    location: "Predli / US - San Francisco Bay Area",
+    bullets: [
+      <>
+        Collaborated with a UI/UX designer to design a User Interface in{" "}
+        <span className="bold">Figma</span> and implemented it using React.js
+        from scratch.
+      </>,
+      <>
+        Worked closely with the teach lead to design a{" "}
+        <span className="bold">Trading System</span> for buying/selling music
+        master rights .
+      </>,
+      <>
+        Implented animations and interactions using{" "}
+        <span className="bold">Framer Motion</span>.
+      </>,
+      <>
+        Implemented redux side effects using{" "}
+        <span className="bold">Redux-Saga</span> to handle asynchronous
+        requests.
+      </>,
+    ],
+  },
+  {
     positionName: "Software Engineer Intern (Remote)",
-    date: "Dec 2021 - Present",
+    date: "Dec 2021 - Feb 2021",
     location: "Mezur / US - California",
     bullets: [
       <>
@@ -34,7 +60,7 @@ const experiencesList = [
       <>
         Designed and implemented a 10+ Web Applications like{" "}
         <span className="bold">E-commerce</span> and{" "}
-        <span className="bold">Online Appointment Booking</span> Apps{" "}
+        <span className="bold">Online Appointment Booking</span> Apps using{" "}
         <span className="bold">React</span>,{" "}
         <span className="bold">Express</span> and{" "}
         <span className="bold">MongoDB</span> for multiple firms across Kolkata,
@@ -50,8 +76,7 @@ const experiencesList = [
       <>
         Ensured only authorized employees have access to the application by
         implementing a authentication using{" "}
-        <span className="bold">JSON Web Token</span>, cookies and sessions to
-        and sign valid requests.
+        <span className="bold">JSON Web Token</span> to sign valid requests.
       </>,
       <>
         Deployed Web Applications to production using{" "}
