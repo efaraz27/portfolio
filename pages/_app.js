2054cb9caf4d6bf454d12f7df461e3fe84a556aa
Ehtesham Faraz <efaraz27@gmail.com>
2023-02-04 04:11:53 +0530

    move clarity script

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import { Provider } from "react-redux";
 import store from "../redux/store";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Provider store={store}>
+        <Script
+          id="clarity"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "djtosfcyca");`,
+          }}
+        />
         <Component {...pageProps} />
       </Provider>
     </div>
--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <Script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "djtosfcyca");`,
-          }}
-        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
