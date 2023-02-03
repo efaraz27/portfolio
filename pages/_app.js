dd7e24010b28f424963c815adc1a22bcb0b4cdc3
Ehtesham Faraz <efaraz27@gmail.com>
2023-02-04 04:18:05 +0530

    remove unused file

--- pages/_app.js
+++ pages/_app.js
@@ -2,22 +2,31 @@ import "../styles/globals.css";
 import { Provider } from "react-redux";
 import store from "../redux/store";
 import Script from "next/script";
+import { useEffect, useRef } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const script = (
+      <Script
+        id="clarity"
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "djtosfcyca");`,
+        }}
+      />
+    );
+    if (ref.current) ref.current.appendChild(script);
+  }, []);
+
   return (
-    <div>
+    <div ref={ref}>
       <Provider store={store}>
-        <Script
-          id="clarity"
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `(function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "djtosfcyca");`,
-          }}
-        />
         <Component {...pageProps} />
       </Provider>
     </div>
