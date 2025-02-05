d939609d3ce70d756a35f16d585c6d2b259dc8ed
efaraz27 <efaraz27@gmail.com>
2025-02-05 22:41:06 +0100

    patch: projects

--- constants/projectsList.js
+++ constants/projectsList.js
@@ -41,22 +41,6 @@ const projectsList = [
     tags: "Next.js - FastAPI - AWS",
     url: "https://www.predli.com",
   },
-  {
-    name: "Piloteer",
-    desc: (
-      <span>
-        Add Piloteer's Sensing AI to Your Meetings.
-        <br />
-        Seamlessly integrate an AI coach into any virtual meeting. 
-        <br />
-        Get live scoring, personalized feedback, and expert coaching on 13 leadership dimensions 
-        to optimize your impact and improve your leadership skills.
-      </span>
-    ),
-    images: "/project-images/piloteer",
-    tags: "React - Next.js - AI - Real-time Processing",
-    url: "https://www.piloteer.com",
-  },
 ];
 
 export default projectsList;
